6d127b09ddeee7b9f6363f2f6bfaf1b35265b01b
Benjamin Sundell <sundell.benjamin@gmail.com>
2022-12-05 21:03:01 +0100

    removed old files

--- javascript/sidebar.js
+++
@@ -1,5 +0,0 @@
-const toggleSidebar = () => {
-  const sidebarItems = document.querySelectorAll('.sidebar-text');
-
-  sidebarItems.forEach((sidebarItem) => sidebarItem.classList.toggle('active'));
-};
