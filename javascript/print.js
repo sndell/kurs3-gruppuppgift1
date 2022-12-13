96b7858df7774aed583617d5cc327f35ce586b4f
Benjamin Sundell <sundell.benjamin@gmail.com>
2022-12-13 02:45:15 +0100

    removing old files

--- javascript/print.js
+++
@@ -1,9 +0,0 @@
-const printDiv = (noteTitle, divId) => {
-  const printContents = document.querySelector('.preview-content');
-  const originalContents = document.body.innerHTML;
-  document.body.innerHTML = '';
-  document.body.append(printContents);
-  document.title = noteTitle;
-  window.print();
-  document.body.innerHTML = originalContents;
-};
